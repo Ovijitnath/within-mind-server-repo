98627824058495991c76c688103b4808fa6be76e
Ovijitnath <ovijitnathgn@gmail.com>
2022-11-10 13:19:51 +0600

    services loadin from db

--- index.js
+++ index.js
@@ -15,11 +15,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m7f1lqz.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        const serviceCollection = client.db('withinMind').collection('services');
+        // const orderCollection = client.db('geniusCar').collection('orders');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 
